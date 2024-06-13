4b1d2d0d21ad35da69340a3dfea59e43b4ce93ad
Dmitry Zakharov <ya.dimaz@gmail.com>
2024-06-13 09:19:40 +0300

    Execute Lection 1

---
+++ Lection_1/program.java
@@ -0,0 +1,15 @@
+package Lection_1;
+
+import java.util.Scanner;
+
+public class program {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("int a: ");
+        boolean flag = iScanner.hasNextInt();
+        System.out.println(flag);
+        int i = iScanner.nextInt();
+        System.out.println(i);
+        iScanner.close();
+    }
+}
