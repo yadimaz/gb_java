4517a43e2ffe44ee1303a0426af00649689a8c66
Dmitry Zakharov <ya.dimaz@gmail.com>
2024-07-05 16:43:24 +0300

    Add comment Pract6

--- Pract6/NotebookMain.java
+++ Pract6/NotebookMain.java
@@ -1,27 +1,35 @@
 package Pract6;
 
-import javax.swing.*;
 import java.util.*;
 
 public class NotebookMain {
     public static void main(String[] args) {
+        // Создаём множество экземпляров класса Notebook
         Notebook ntb1 = new Notebook("Acer", "Aspire 3 A315-44P-R7ZT", 1.78, 15.6, "AMD Ryzen 5", 8, 512, "без ОС",
                 "серебряный");
-        Notebook ntb2 = new Notebook("Lenovo", "ThinkPad X280", 1.13, 12.5, "AIntel Core i5 8250U", 8, 256, "Windows 10 Pro",
+        Notebook ntb2 = new Notebook("Lenovo", "ThinkPad X280", 1.13, 12.5, "AIntel Core i5 8250U", 8, 256,
+                "Windows 10 Pro",
                 "черный");
-        Notebook ntb3 = new Notebook("Xiaomi", "Book Pro 16", 1.8, 16, "Intel Core i5 1240p", 16, 512, "Windows 11 Home",
+        Notebook ntb3 = new Notebook("Xiaomi", "Book Pro 16", 1.8, 16, "Intel Core i5 1240p", 16, 512,
+                "Windows 11 Home",
                 "черный");
-        Notebook ntb4 = new Notebook("ASUS", "Vivobook Go E1504FA-BQ585", 1.63, 15.6, "AMD Ryzen 3 7320U", 8, 256, "без ОС",
+        Notebook ntb4 = new Notebook("ASUS", "Vivobook Go E1504FA-BQ585", 1.63, 15.6, "AMD Ryzen 3 7320U", 8, 256,
+                "без ОС",
                 "черный");
 
         Set<Notebook> ntbks = new HashSet<>(Arrays.asList(ntb1, ntb2, ntb3, ntb4));
 
+        // Запрос условий фильтрации
         Map<Integer, String> fltrs = getFilters();
-        Set<Notebook> ntbksFiltered = search(ntbks,fltrs);
+        // Поиск с учетом фиьтров
+        Set<Notebook> ntbksFiltered = search(ntbks, fltrs);
 
-        if (ntbksFiltered.isEmpty()) System.out.println("По данным параметрам >" + fltrs +"< ничего не найдено");
+        // Вывод результата
+        if (ntbksFiltered.isEmpty())
+            System.out.println("\nПо данным параметрам >" + fltrs + "< ничего не найдено");
         else {
-            //System.out.println(ntbksFiltered);
+            // System.out.println(ntbksFiltered);
+            System.out.println("\nВот что нашлось: \n");
             Iterator note = ntbksFiltered.iterator();
             while (note.hasNext()) {
                 System.out.println(note.next() + "\n");
@@ -31,29 +39,30 @@ public class NotebookMain {
 
     }
 
-    public static Set<Notebook> search (Set<Notebook> ntbks, Map<Integer, String> fltrs) {
+    public static Set<Notebook> search(Set<Notebook> ntbks, Map<Integer, String> fltrs) {
         Set<Notebook> ntbksFiltered = new HashSet<>();
 
-        Double fltrsWeigth  = Double.parseDouble(fltrs.get(1));
-        Double fltrsScreen  = Double.parseDouble(fltrs.get(2));
-        Integer fltrsMem  = Integer.parseInt(fltrs.get(3));
-        Integer fltrsHdd  = Integer.parseInt(fltrs.get(4));
+        Double fltrsWeigth = Double.parseDouble(fltrs.get(1));
+        Double fltrsScreen = Double.parseDouble(fltrs.get(2));
+        Integer fltrsMem = Integer.parseInt(fltrs.get(3));
+        Integer fltrsHdd = Integer.parseInt(fltrs.get(4));
 
-        System.out.println("\nВот что нашлось: \n");
-        for(Notebook notebook : ntbks) {
+        // Фильтруем и добавляем в ntbksFiltered
+        for (Notebook notebook : ntbks) {
             if (notebook.getWeight() >= fltrsWeigth && notebook.getScreenDiag() >= fltrsScreen &&
                     notebook.getMemory() >= fltrsMem && notebook.gethdd() >= fltrsHdd) {
-                if(fltrs.containsKey(5)) {
-                    if (notebook.getColor().equals(fltrs.get(5).toUpperCase())) ntbksFiltered.add(notebook);
-                }
-                else ntbksFiltered.add(notebook);
+                if (fltrs.containsKey(5)) {
+                    if (notebook.getColor().equals(fltrs.get(5).toUpperCase()))
+                        ntbksFiltered.add(notebook);
+                } else
+                    ntbksFiltered.add(notebook);
             }
         }
 
         return ntbksFiltered;
     }
 
-    public static Map<Integer, String> getFilters () {
+    public static Map<Integer, String> getFilters() {
         Map<Integer, String> filters = new HashMap<>();
 
         Map<Integer, String> menu = new HashMap<>();
@@ -64,8 +73,9 @@ public class NotebookMain {
         menu.put(5, "Цвет корпуса");
 
         Scanner scanner = new Scanner(System.in);
-
         boolean flag = true;
+
+        // Запрос параметров
         while (flag) {
             System.out.println("1 - Вес\n2 - Диагональ матрицы\n" +
                     "3 - Объём памяти\n4 - Размер жесткого диска\n5 - Цвет\nВведите цифру, соответствующую необходимому критерию: \n");
@@ -76,9 +86,9 @@ public class NotebookMain {
             if (menu.containsKey(filterKey)) {
                 System.out.println("Введите значение " + menu.get(filterKey));
                 String filterValue = scanner.next();
-                filters.put(filterKey,filterValue);
-            }
-            else System.out.println("Неверное значение категории!!!");
+                filters.put(filterKey, filterValue);
+            } else
+                System.out.println("Неверное значение категории!!!");
 
             // Вывод текущих значений фильтра
             System.out.println("Текущие значения фильтра: " + filters);
@@ -86,7 +96,8 @@ public class NotebookMain {
             // Запрос на окончание цикла
             System.out.println("Задать следующий критерий поиска? 1 - да, 2 - нет");
             int oneTwo = scanner.nextInt();
-            if (oneTwo == 2) flag = false;
+            if (oneTwo == 2)
+                flag = false;
         }
 
         // Заполнение отсутствующих парамеров 0
@@ -96,8 +107,7 @@ public class NotebookMain {
             } else {
                 try {
                     Double.parseDouble(filters.get(i));
-            }
-                catch (Exception e) {
+                } catch (Exception e) {
                     filters.put(i, "0");
                 }
             }
